e3cd0cea6ba67cd85ac6855d1f8faa117311f8b3
AshwinLechnerRea <107248882+AshwinLechnerRea@users.noreply.github.com>
2022-07-12 13:42:33 +0200

    Added opdracht 4 Objecten

---
+++ Objecten/opdracht4/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once('classes/Mareep.php');
+$mareep1 = new Mareep(40);
+$mareep2 = new Mareep(60);
+$mareep3 = new Mareep(10);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdracht 4</title>
+</head>
+
+<body>
+    <p> <?=$mareep1->tackle()?></p>
+    <p> <?=$mareep2->tackle()?></p>
+    <p><?=$mareep3->tackle()?></p>
+    <p><?=$mareep3->groupAttack()?></p>
+</body>
+
+</html>
