22834ec9f0c166421b53fd24dc7341278a90e9c2
AshwinLechnerRea <107248882+AshwinLechnerRea@users.noreply.github.com>
2022-07-13 13:17:53 +0200

    Added opdracht3 Overerven

---
+++ Overerving/opdracht3/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once('classes/Animal.php');
+$henk = new Human();
+$henk->setAge(47);
+$henk->setEducation('REA College');
+$henk->setJob('Programmer');
+$mika = new Dog();
+$mika->setAge(7);
+$mika->setBreed('German Shepherd')
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdracht 3</title>
+</head>
+
+<body>
+    <?= $henk->celebrateBirthday() ?>
+    <?= $henk->celebrateBirthday() ?>
+    <?= $henk->celebrateBirthday() ?>
+    <br>
+    <?= $mika->celebrateBirthday() ?>
+</body>
+
+</html>
